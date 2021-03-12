d5b53a745fe5111f3e26a20bbca6a8568424da8b
adri <adri@192.168.0.12>
2021-03-12 01:30:40 +0100

    test Suma Producto

--- AdrianRodriguezMontesinos_Actividad2/src/test/TestOperaciones.java
+++ AdrianRodriguezMontesinos_Actividad2/src/test/TestOperaciones.java
@@ -1,10 +1,14 @@
 package test;
+import operaciones.*;
 
 public class TestOperaciones {
 
 	public static void main(String[] args) {
-		System.getproperty("java.classpath");
-
+		Suma s = new Suma(2,3);
+		Producto p = new Producto(3,4);
+		System.out.println("El resultado de la suma es: " + s.sumaEnteros());
+		System.out.println("El resultado del producto es: " + p.productoEnteros());
+		
 	}
 
 }
